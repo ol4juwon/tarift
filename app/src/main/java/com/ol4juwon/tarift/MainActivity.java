81392b5f9d9c0751243d59a544ca6cfcb010815e
ol4juwon <56265954+ol4juwon@users.noreply.github.com>
2020-10-20 00:51:44 +0100

    activitivy comit

---
+++ app/src/main/java/com/ol4juwon/tarift/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ol4juwon.tarift;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+
+    }
+}
